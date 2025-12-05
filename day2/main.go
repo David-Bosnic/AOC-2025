f367ad550ecdcfeeb27698cf53f4ddbd6be8e7b2
David Bosnic <davidbosnic99@gmail.com>
2025-12-05 14:30:09 -0700

    day2 part 1

--- day2/main.go
+++ day2/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Bound struct {
@@ -12,6 +13,18 @@ type Bound struct {
 }
 
 func main() {
+	bound := readFile()
+	var count int
+	for i := range bound {
+		fmt.Printf("bound[32].higher: %v\n", bound[31].higher)
+		for j := bound[i].lower; j <= bound[i].higher; j++ {
+			currentNumber := j
+			if isInvalidString(strconv.Itoa(j)) {
+				count += currentNumber
+			}
+		}
+	}
+	fmt.Printf("count: %v\n", count)
 }
 
 func readFile() []Bound {
@@ -20,5 +33,24 @@ func readFile() []Bound {
 		fmt.Println("Fail to read file")
 		os.Exit(1)
 	}
-	fmt.Println(file)
+	var bound []Bound
+	parts := strings.Split(string(file), ",")
+	for i := range parts {
+		lower, higher, _ := strings.Cut(parts[i], "-")
+		lowerBound, _ := strconv.Atoi(lower)
+		higherBound, _ := strconv.Atoi(higher)
+		bound = append(bound, Bound{
+			lower:  lowerBound,
+			higher: higherBound,
+		})
+	}
+	return bound
+}
+
+func isInvalidString(s string) bool {
+	l := len(s)
+	if l%2 != 0 {
+		return false
+	}
+	return s[:l/2] == s[l/2:]
 }
